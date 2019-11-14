1ae5c9da60c21586ad41e5aec65ec7c295617607
Jonathan Ray <jray@careevolution.com>
2019-11-14 16:49:53 -0600

    Rename data-names to singular

--- src/test/rules/1.9/data-name-with-periods.ts
+++ src/test/rules/1.9/data-name-with-periods.ts
@@ -1,10 +1,10 @@
-import { rule } from '../../../rules/1.9/data-names-with-periods';
+import { rule } from '../../../rules/1.9/data-name-with-periods';
 import { RuleTester } from 'eslint';
 
 const ruleTester = new RuleTester();
 const message = 'events not fired by .data("name.with.periods") in jQuery 1.9';
 
-ruleTester.run('data-names-with-periods', rule, {
+ruleTester.run('data-name-with-periods', rule, {
     valid: [
         '$("").data("something")'
     ],
